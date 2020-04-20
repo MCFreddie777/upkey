cfcb1d9ef898ec7d957a7b33744cab18dfc065ae
František Gič <frantisek.gic@gmail.com>
2020-04-20 20:16:14 +0200

    Implement function for checking for updates from a log

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,12 @@
 import fs from 'fs';
 import { Server, Stb } from './types';
+import { exec } from 'child_process';
+import readline from 'readline';
 
 // Config
 const serversFilePath = 'servers.json';
 const stbFilePath = 'stb.json';
+const logFilePath = '/usr/local/etc/log.txt';
 
 const servers: Server[] = [];
 let stb: Stb;
@@ -17,9 +20,11 @@ const loadServer = (serversFilePath: string): void => {
         throw Error(`Error: Servers file missing. (${serversFilePath})`);
     }
 
-    JSON.parse(file.toString()).forEach((server: { name: string; group: number; url: string }) => {
-        servers.push({ ...server, update: false });
-    });
+    JSON.parse(file.toString()).forEach(
+        (server: { name: string; group: number; url: string }) => {
+            servers.push({ ...server, update: false });
+        }
+    );
 };
 
 const loadStb = (stbFilePath: string): void => {
@@ -34,5 +39,34 @@ const loadStb = (stbFilePath: string): void => {
     stb = JSON.parse(file.toString());
 };
 
+const check = (logFilePath: string, ftp: boolean = false) => {
+    // Get the logfile from remote
+    if (ftp) {
+        exec(
+            `wget ftp://${stb.ip}/usr/keys/log.txt -q --ftp-user ${stb.ftpUsername} --ftp-password ${stb.ftpPassword} && chmod 777 log.txt`
+        );
+    }
+
+    const rd = readline.createInterface(fs.createReadStream(logFilePath));
+
+    rd.on('line', (line) => {
+        servers.forEach((s) => {
+            if (line.includes(`${s.name}: login failed, usr/pwd invalid'`))
+                s.update = true;
+        });
+    }).on('close', () => {
+        // Remove from remote
+        if (ftp) {
+            exec(
+                `rm -rf log.tx* && sshpass -p ${stb.ftpPassword} ssh -l ${stb.ftpUsername} ${stb.ip} "> /usr/keys/log.txt"`
+            );
+        }
+
+        // Clear the file
+        exec(` > ${logFilePath}`);
+    });
+};
+
 loadServer(serversFilePath);
 loadStb(stbFilePath);
+check(logFilePath);
